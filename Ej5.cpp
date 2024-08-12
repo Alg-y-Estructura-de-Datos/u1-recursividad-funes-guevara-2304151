4301cc9189697ee50984f1736636a6eb001e40b3
AnaRFG <2304151@ucc.edu.ar>
2024-08-12 15:55:44 -0300

    ejercicio 5 y 6, reveer ultimas cuestiones de la funcion

--- Ej5.cpp
+++ Ej5.cpp
@@ -1,3 +1,37 @@
-//
-// Created by Usuario on 12/8/2024.
-//
+#include <iostream>
+using namespace std;
+
+bool contieneDigito(int n, int d){
+    //caso base 1
+    if (n == 0){
+        return false;
+    }
+
+        // caso base 2, Si el dígito actual (n % 10) es igual a 'd', retornar verdadero
+        if (n % 10 == d) {
+            return true;
+        }
+        // Llamada recursiva con el número sin el último dígito
+        return contieneDigito(n / 10, d);
+}
+
+
+
+int main(){
+    int n, d;
+    cout << "ingrese un numero\n";
+    cin >> n;
+    cout << "ingrese un digito\n";
+    cin >> d;
+    // Verificar si el dígito está en el número usando la función recursiva
+    bool resultado = contieneDigito(n, d);
+    if (resultado) {
+        cout << "El digito " << d << " esta en el numero " << n << "." << endl;
+    } else {
+        cout << "El digito " << d << " no esta en el numero " << n << "." << endl;
+    }
+
+    return 0;
+}
+
+
--- Ej6.cpp
+++ Ej6.cpp
@@ -1,3 +1,33 @@
-//
-// Created by Usuario on 12/8/2024.
-//
+#include <iostream>
+using namespace std;
+
+// Función recursiva para contar dígitos pares en posiciones impares
+int contarParesEnPosicionesImpares(int n, int tamNro ) {
+    // Caso base: si n es 0, no hay más dígitos que revisar
+    if (n == 0) {
+        return 0;
+    }
+    // Verificar si la posición actual es impar y el dígito es par
+    int digitoActual = n % 10;
+    if (tamNro % 2 != 0 && digitoActual % 2 == 0) {
+        // Incrementar el contador y realizar la llamada recursiva
+        return 1 + contarParesEnPosicionesImpares(n / 10, tamNro - 1); //si pasa que true y true suma 1,
+    } else {
+        // Realizar la llamada recursiva sin incrementar el contador
+        return contarParesEnPosicionesImpares(n / 10, tamNro - 1);
+    }
+}
+
+int main(){
+    int n, d;
+    cout << "Ingrese la cantidad de digitos\n";
+    cin >> d;
+    cout << "Ingrese un numeros entero positivo:\n";
+    cin >> n;
+
+    // Contar dígitos pares en posiciones impares usando la función recursiva
+    int resultado = contarParesEnPosicionesImpares(n,d);
+
+    cout << "Cantidad de digitos pares en posiciones impares: " << resultado << endl;
+}
+
